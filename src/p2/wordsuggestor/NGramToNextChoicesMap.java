00872f3894e4ec0aaba90a16f2f2634302d0bed7
Justin Wang <wang.justin03@gmail.com>
2018-01-23 22:42:40 -0800

    c1 part 1 finish

--- src/p2/wordsuggestor/NGramToNextChoicesMap.java
+++ src/p2/wordsuggestor/NGramToNextChoicesMap.java
@@ -1,11 +1,13 @@
 package p2.wordsuggestor;
 
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.function.Supplier;
 
 import cse332.datastructures.containers.Item;
 import cse332.exceptions.NotYetImplementedException;
 import cse332.interfaces.misc.Dictionary;
+import cse332.interfaces.misc.SimpleIterator;
 import cse332.misc.LargeValueFirstItemComparator;
 import cse332.sorts.InsertionSort;
 import cse332.types.AlphabeticString;
@@ -26,7 +28,16 @@ public class NGramToNextChoicesMap {
      * Increments the count of word after the particular NGram ngram.
      */
     public void seenWordAfterNGram(NGram ngram, String word) {
-        throw new NotYetImplementedException();
+        AlphabeticString aWord = new AlphabeticString(word);
+        if (map.find(ngram) == null) { // if ngram is not there
+            map.insert(ngram, newInner.get()); // insert ngram
+        } else { // if ngram is there
+            if (map.find(ngram).find(aWord) == null) { // word is not there
+                map.find(ngram).insert(aWord, 1);
+            } else { // word is there, then increment by one
+                map.find(ngram).insert(aWord, map.find(ngram).find(aWord) + 1);
+            }
+        }
     }
 
     /**
@@ -39,7 +50,16 @@ public class NGramToNextChoicesMap {
      * @return An array of all the Items for the requested ngram.
      */
     public Item<String, Integer>[] getCountsAfter(NGram ngram) {
-        throw new NotYetImplementedException();
+        if (map.find(ngram) == null) {
+            return new Item[0];
+        }
+        Item<String, Integer>[] counts = new Item[map.find(ngram).size()]; //create array for size of ngram
+        Iterator<Item<AlphabeticString, Integer>> iter = map.find(ngram).iterator();
+        for (int i = 0; i < map.find(ngram).size(); i++) { //loop through the things
+            Item<AlphabeticString, Integer> item = iter.next();
+            counts[i] = new Item<String, Integer>(item.key.toString(), item.value);
+        }
+        return counts;
     }
 
     public String[] getWordsAfter(NGram ngram, int k) {
